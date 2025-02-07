54c19691e41b8c7fff88a82af51707228dbff99e
Grrcool <aidenwonggarcia2009@gmail.com>
2025-02-07 18:35:28 -0500

    Added Light and working on menu
    Light particle thingamajig needs to add the fading code and fix the code. Working on menu cause too lazy to work on the fading code.

--- AG - Unfolding Realms/Assets/Scripts/FadingLight.cs
+++ AG - Unfolding Realms/Assets/Scripts/FadingLight.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class FadingLight : MonoBehaviour
 {
+    private ParticleSystem BrightLight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BrightLight.Play();
     }
 
     // Update is called once per frame
